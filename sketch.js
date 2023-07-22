e53e804735de6597b8a8849c382fe70434283207
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-07-22 09:05:18 +0000

    primer ejercicio facilito

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
+var data;
+
 function preload() {
-    data = loadjson("birds.json");
-}
-
-function setup() {
-
+  
+  data = loadJSON("birds.json")
 }
 
 function draw(){
+  noCanvas();
+  var bird = data.birds[1].members[2]
+  createP(bird);
     
 }
