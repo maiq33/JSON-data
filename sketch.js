7a024c1b2eb69c6367172d6b014aee6bc248070e
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-07-23 08:10:23 +0000

    commit final

--- sketch.js
+++ sketch.js
@@ -7,7 +7,21 @@ function preload() {
 
 function draw(){
   noCanvas();
-  var bird = data.birds[1].members[2]
-  createP(bird);
+  //var bird = data.birds[1].members[2]
+  //createP(bird);
+
+  var birds = data.birds;
+
+  
+
+  for ( var i = 0; i < birds.length; i++){
+    createElement('h1', birds[i].family);
+    var members = birds[i].members;
+    for (let j = 0; j < birds.length; j++) {
+      createDiv(members[j]);
+      
+    }
+  
+  }
     
 }
